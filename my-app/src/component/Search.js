cb81f165c7f0cf74062c26ca0148963aab94bd83
jianliu200 <jiangyuanliu2002@gmail.com>
2022-03-26 18:44:21 -0500

    front end

---
+++ my-app/src/component/Search.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Search(){
+    return(
+        <section>
+            <div>
+                <h1>Welcome to Auctioning from the 1990</h1>
+                <p>Begin by searching up what you want from your 90s nostagia</p>
+            </div>
+        </section>
+
+    
+    );
+}
+
+export default Search;
